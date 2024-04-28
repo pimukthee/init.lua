73f7ade17ce19fb27f822e554249b30a71752dcf
pimukthee <pimukthee@gmail.com>
2024-04-28 13:45:47 +0700

    add clipboard remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -6,13 +6,15 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("x", "p", '"_dP')
+vim.keymap.set("x", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("x", "<leader>d", '"_d')
 
 vim.keymap.set("n", "<leader>rp", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>w", ':lua vim.diagnostic.open_float(0, { scope = "line" })<CR>')
 
 vim.keymap.set("n", "<Esc>", "<cmd> noh <CR>")
-
 
 vim.keymap.set("n", "<leader>tn", "<cmd> bnext <CR>")
 vim.keymap.set("n", "<leader>tp", "<cmd> bprev <CR>")
