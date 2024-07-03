424b8a9592a39f4bc173f1791d698c34a5b11007
pimukthee <pimukthee@gmail.com>
2024-07-03 12:38:17 +0700

    change line number colour

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,5 +3,9 @@ return {
 	config = function()
 		require("catppuccin").setup({})
 		vim.cmd("colorscheme catppuccin-macchiato")
+
+        vim.api.nvim_set_hl(0, 'LineNrAbove', {fg='grey'})
+        vim.api.nvim_set_hl(0, 'LineNr', {fg='grey'})
+        vim.api.nvim_set_hl(0, 'LineNrBelow', {fg='grey'})
 	end,
 }
