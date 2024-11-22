5e95c7a96fd353d750ceebbaa66c6d3f99b45e4d
pimukthee <pimukthee@gmail.com>
2024-11-22 17:52:19 +0700

    enable hidden file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,23 @@
 return {
-   "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-    end
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+            pickers = {
+                find_files = {
+                    find_command = {
+                        "rg",
+                        "--files",
+                        "--hidden",
+                        "--glob",
+                        "!.git/*",
+                    }
+                }
+            },
+		})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+	end,
 }
